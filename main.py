1115313a8b30e30bac1c83d24397eccd01a6372e
Adam Olech <nddr89@gmail.com>
2021-04-21 21:41:46 +0200

    main.py: load images as uint8

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from skimage import io
-path = "image/Kropka.jpg"
-img = io.imread(path, as_gray=False)
-print("Image: " + path)
-print(img[250])
-print("The end")
+from skimage import io, img_as_ubyte
+
+dot_path = "images/Kropka.jpg"
+ct_path = "images/CT_ScoutView.jpg"
+
+dot_img = img_as_ubyte(io.imread(dot_path, as_gray=True))
+ct_img = img_as_ubyte(io.imread(ct_path, as_gray=True))
+
+print(dot_img[250])
