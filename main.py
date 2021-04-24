82b0adcc90605aa6d401559175894506d360fb86
Aleksandra Wojewoda <a.wojewoda18@gmail.com>
2021-04-24 20:21:59 +0200

    Manualy way to calculate points

--- main.py
+++ main.py
@@ -23,29 +23,29 @@ avg=int(sum_det/height)
 print("Avg value of pixel for basic detector: " + str(avg))
 print("---------------------------------------------")
 #coordinates
-tmp_tab = draw.line_nd((0,0),((height-1),(width-1)),endpoint=True)
+tmp_tab = draw.line_nd((height/2,-width/2),(-(height-2)/2,(width-2)/2),endpoint=True)
 
 value_tab = []
 tab_len = len(tmp_tab[0])
-print("tab_len is equil: " + str(tab_len))
+print(f'tab_len is equal {tab_len}')
+#print(tmp_tab)
 i = 0
 while i < tab_len:
 	tmp = []
 	tmp.append(tmp_tab[0][i])
 	tmp.append(tmp_tab[1][i])
 	value_tab.append(tmp)
-	#is in python i++ or just i+=1???
 	i+=1
 #in value_tab are tables with coordinates points to count values in the curve
 print(value_tab)
 
-print("---------------------------------------------")
+#print("---------------------------------------------")
 
-for ele in value_tab:
-	img[ele[0],ele[1]] = 255
-
-io.imsave(img_path + ".diag.jpg",img)
-print("Img " + img_path + ".diag.jpg" + " saved.")
+#for ele in value_tab:
+#	img[ele[0],ele[1]] = 255
+#
+#io.imsave(img_path + ".diag.jpg",img)
+#print("Img " + img_path + ".diag.jpg" + " saved.")
 
 print("---------------------------------------------")
 #width = len(img[0])
@@ -53,5 +53,43 @@ print("---------------------------------------------")
 
 r = (math.sqrt(width**2+height**2))/2
 print("Radius is equil " + str(r))
+
+#point on circle
+pt_start = []
+pt_stop = []
+#angle; 180 == np.pi; 90 == np.pi/2 ect
+alfa = np.pi
+x_1 = int(r * math.cos(alfa))
+y_1 = int(r * math.sin(alfa))
+pt_start.append(x_1)
+pt_start.append(y_1)
+x_2 = int(r * math.cos(alfa-np.pi))
+y_2 = int(r * math.sin(alfa-np.pi))
+pt_stop.append(x_2)
+pt_stop.append(y_2)
+print(f'pt_start is equal {pt_start}')
+print(f'pt_stop is equal {pt_stop}')
+
+
+#coordinates
+tmp_tab = draw.line_nd(tuple(list(pt_start)),tuple(list(pt_stop)),endpoint=True)
+
+value_tab = []
+tab_len = len(tmp_tab[0])
+print(f'tab_len is equal {tab_len}')
+#print(tmp_tab)
+i = 0
+while i < tab_len:
+	tmp = []
+	tmp.append(tmp_tab[0][i])
+	tmp.append(tmp_tab[1][i])
+	value_tab.append(tmp)
+	i+=1
+#in value_tab are tables with coordinates points to count values in the curve
+print(value_tab)
+
+
+
+print("---------------------------------------------")
 
 
