a76dd1477b638a8af318dcf21d677432836cf624
Aleksandra Wojewoda <a.wojewoda18@gmail.com>
2021-04-25 18:05:36 +0200

    Problem with vector solved.

--- main.py
+++ main.py
@@ -53,17 +53,6 @@ def count_pt_for_one_scan(start_angle, gamma, n, r):
 	result_tab_em = []
 	start = start_angle - gamma/2
 	stop = start_angle - 180 - gamma/2
-
-	#alfa = np.pi
-	#x_1 = int(r * math.cos(alfa))
-	#y_1 = int(r * math.sin(alfa))
-	#pt_start.append(x_1)
-	#pt_start.append(y_1)
-	#x_2 = int(r * math.cos(alfa-np.pi))
-	#y_2 = int(r * math.sin(alfa-np.pi))
-	#pt_stop.append(x_2)
-	#pt_stop.append(y_2)
-
 	while n > 0:
 		y_1 = int(r * math.sin(math.radians(start)))
 		x_1 = int(r * math.cos(math.radians(start)))
@@ -80,6 +69,18 @@ def count_pt_for_one_scan(start_angle, gamma, n, r):
 	#print(f'result_tab_em is equal {result_tab_em}')
 	return [result_tab_det, result_tab_em]
 
+
+#switch frame of reference (19,-10) --> (0,0)
+def signed_trace_to_unsigned_trace(pts_tab_tup, h, w):
+	result = []
+	for ele in pts_tup:
+		y_tmp = abs(ele[0]-(h//2-1))
+		x_tmp = ele[1]+w//2
+		tmp_tup = y_tmp,x_tmp
+		result.append(tmp_tup)
+	return result
+
+
 img_path = sys.argv[1]
 
 img = img_as_ubyte(io.imread(img_path, as_gray=True))
@@ -89,100 +90,21 @@ width = len(img[0])
 height = len(img)
 print("Width of " + img_path + " is: " + str(width))
 print("Height of " + img_path + " is: " + str(height))
-print("Middle element is " + str(int(width/2)))
-sum_det = 0
-for ele in img:
-	sum_det += ele[int(width/2)]
 
-print("Sum of pixels value for basic detector: " + str(sum_det))
-avg=int(sum_det/height)
-print("Avg value of pixel for basic detector: " + str(avg))
-print("---------------------------------------------")
-#coordinates
-tmp_tab = draw.line_nd((height/2,-width/2),(-(height-2)/2,(width-2)/2),endpoint=True)
-
-value_tab = line_to_y_x_list(tmp_tab)
-
-tab_len = len(tmp_tab[0])
-print(f'tab_len is equal {tab_len}')
-#print(tmp_tab)
-#in value_tab are tables with coordinates points to count values in the curve
-print(value_tab)
-
-#print("---------------------------------------------")
-
-#for ele in value_tab:
-#	img[ele[0],ele[1]] = 255
-#
 #io.imsave(img_path + ".diag.jpg",img)
 #print("Img " + img_path + ".diag.jpg" + " saved.")
-
-print("---------------------------------------------")
-#width = len(img[0])
-#height = len(img)
-
 r = (math.sqrt(width**2+height**2))/2
-print("Radius is equil " + str(r))
+print("radius is equal " + str(r))
 
-result = count_pt_for_one_scan(180, 8, 5, r)
-print(f'result is equal {result}')
+#result = count_pt_for_one_scan(180, 10, 3, r)
+#tmp result for test function signed_trace_to_unsigned_trace
+result = [[(19, -10)],[(-20,9)]]
+print(f'result is equal : {result}')
 
-#point on circle
-pt_start = []
-pt_stop = []
-#angle; 180 == np.pi; 90 == np.pi/2 ect
-alfa = np.pi
-x_1 = int(r * math.cos(alfa))
-y_1 = int(r * math.sin(alfa))
-pt_start.append(y_1)
-pt_start.append(x_1)
-x_2 = int(r * math.cos(alfa-np.pi))
-y_2 = int(r * math.sin(alfa-np.pi))
-pt_stop.append(y_2)
-pt_stop.append(x_2)
-print(f'pt_start is equal {pt_start}')
-print(f'pt_stop is equal {pt_stop}')
+solved = edp_trace(result, height, width)
+print(f'solved is equal : {solved}')
 
-print(type(pt_start))
-print(type(pt_stop))
+done = signed_trace_to_unsigned_trace(solved[0], height, width)
+print(f'done is equal : {done}')
 
-#coordinates
-tmp_tab = draw.line_nd(tuple(pt_start),tuple(pt_stop),endpoint=True)
-
-value_tab = line_to_y_x_list(tmp_tab)
-tab_len = len(tmp_tab[0])
-print(f'tab_len is equal {tab_len}')
-#print(tmp_tab)
-#in value_tab are tables with coordinates points to count values in the curve
-print(value_tab)
-
-
-
-print("---------------------------------------------")
-
-print("for example to discuse function to take points")
-#coordinates
-height = 8
-width = 6
-tmp_tab = draw.line_nd((3,-4),(-5,2),endpoint=True)
-
-value_tab = []
-tab_len = len(tmp_tab[0])
-print(f'tab_len is equal {tab_len}')
-#print(tmp_tab)
-i = 0
-while i < tab_len:
-	tmp = []
-	tmp.append(tmp_tab[0][i])
-	tmp.append(tmp_tab[1][i])
-	value_tab.append(tmp)
-	i+=1
-#value_tab contains tables with coordinates points to count values in the curve
-print(value_tab)
-
-dummy_cpfos = [[(3,-4)],[(-5,2)]]
-
-dummy_edp_trace = edp_trace(dummy_cpfos, height, width)
-print(dummy_edp_trace)
-
-#print("---------------------------------------------")
+print(50*"-")
